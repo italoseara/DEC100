d95953d840d365d50dbb3514179768c1624abc3b
lluige <luige030301@gmail.com>
2025-11-25 16:31:15 -0300

    Transactions Blocks

--- src/connection/server.py
+++ src/connection/server.py
@@ -1,6 +1,7 @@
 import socket
 import logging
 import threading
+import os
 
 
 class Server:
@@ -10,6 +11,14 @@ class Server:
         self.is_running = False
 
         self.accounts = {}  # Dictionary to store account balances
+
+        self.transactions = []     # store transactions before creating a block
+        self.block_id = 0          # initial ID
+        self.block_file = "transactions_blocks.log"
+
+        if os.path.exists(self.block_file): # clear the archive in each excecution. Idk if it's that way
+            os.remove(self.block_file)
+
 
     def start(self) -> None:
         """Start the server and begin listening for connections."""
@@ -35,6 +44,29 @@ class Server:
             client_socket, addr = self.server_socket.accept()
             logging.info(f"Accepted connection from {addr}")
             threading.Thread(target=self._handle_client, args=(client_socket,), daemon=True).start()
+
+    def _create_block(self):
+        """Create a new transaction block and write it to the block file."""
+
+        block = []
+        block.append(str(self.block_id))
+
+        for t in self.transactions:
+            sign = "-" if t["type"] == "WITHDRAW" else "+"
+            block.append(f"T {t['account']} {sign}{t['value']}")
+        for acc, balance in self.accounts.items():
+            sign = "+" if balance >= 0 else ""
+            block.append(f"S {acc} {sign}{balance}")
+
+        with open(self.block_file, "a") as f:
+            for line in block:
+                f.write(line + "\n")
+            f.write("\n")
+
+        logging.info(f"Block {self.block_id} created with transactions: {self.transactions}")
+
+        self.block_id += 1
+        self.transactions = []
 
     def _handle_client(self, client_socket: socket.socket) -> None:
         """Handle communication with a connected client."""
@@ -82,6 +114,12 @@ class Server:
                             if self.accounts[account_id] >= int(amount):
                                 self.accounts[account_id] -= int(amount)
                                 response = f"Withdrew {amount} from account {account_id}. New balance is {self.accounts[account_id]}."
+
+                                self.transactions.append({"account": account_id, "type": "WITHDRAW", "value": int(amount)})
+
+                                if len(self.transactions) == 6:
+                                    self._create_block()
+
                             else:
                                 response = f"Insufficient funds in account {account_id}."
                         else:
@@ -95,6 +133,12 @@ class Server:
                         if account_id in self.accounts:
                             self.accounts[account_id] += int(amount)
                             response = f"Deposited {amount} to account {account_id}. New balance is {self.accounts[account_id]}."
+
+                            self.transactions.append({"account": account_id, "type": "DEPOSIT", "value": int(amount)})
+
+                            if len(self.transactions) == 6:
+                                self._create_block()
+
                         else:
                             response = f"Account {account_id} does not exist."
                             
